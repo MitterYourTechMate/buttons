fdb8c678d6c8bbca29cf22f05d7272df4127e7fe
shankey raheja <shankeyraheja@gmail.com>
2023-03-08 01:26:07 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import Button from "@mui/material/Button"
+import DeleteIcon from "@mui/icons-material/Delete"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>DEFAULT</Button>
+      <Button variant="outlined">OUTLINED</Button>
+      <Button variant="contained">CONTAINED</Button>
+      <Button variant="contained" color="secondary">
+        SECONDARY
+      </Button>
+      <Button variant="contained" color="success">
+        SUCCESS
+      </Button>
+      <Button variant="contained" color="error">
+        ERROR
+      </Button>
+      <Button variant="contained" color="error" size="small">
+        SMALL
+      </Button>
+      <Button
+        variant="contained"
+        color="error"
+        size="small"
+        startIcon={<DeleteIcon />}
+      >
+        DELETE
+      </Button>
+      <Button
+        variant="contained"
+        color="error"
+        size="small"
+        startIcon={<DeleteIcon />}
+      ></Button>
+      <Button
+        variant="contained"
+        color="error"
+        size="small"
+        endIcon={<DeleteIcon />}
+      >
+        DELETE
+      </Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
